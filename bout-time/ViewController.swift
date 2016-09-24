da38938bfbb2215d2b264c43b8d0b7ff9da17c6b
kyleyu <kyleyu.work@gmail.com>
2016-09-24 09:09:02 -0700

    Initial Commit

---
+++ bout-time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bout-time
+//
+//  Created by Yi YU on 9/24/16.
+//  Copyright © 2016 Yi YU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
